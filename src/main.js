c011128990b9a365a46e3dbc0da87498dea8e506
Oleg Novitsky <darkmus@tut.by>
2020-01-27 09:26:50 +0300

    Fix comments render

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 import MoviesModel from './models/movies';
 import API from './api.js';
-import {generateComments} from './mock/comments';
 import {PageController} from './controllers/page';
 
 const URL = `https://htmlacademy-es-10.appspot.com/cinemaddict/`;
 const AUTHORIZATION_CODE = `Basic eo0w590ik29889a`;
-const commentsData = generateComments();
 
 const api = new API(URL, AUTHORIZATION_CODE);
 const moviesModel = new MoviesModel();
+const pageController = new PageController(document.body, moviesModel);
 
 api.getMovies()
   .then((movies) => {
     moviesModel.movies = movies;
-    return movies;
+    return Promise.all(movies.map((movie) => api.getComments(movie.id)));
   })
-  .then((movies) => Promise.all(movies.map((movie) => api.getComments(movie.id))))
-  .then((comments) => console.log(comments));
+  .then((comments) => {
+    moviesModel.comments = comments.flat();
+    pageController.render();
+  });
