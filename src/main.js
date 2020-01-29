c84adf7fec50cf948b05b1c1a6d5a0a89503d8b9
Oleg Novitsky <darkmus@tut.by>
2020-01-29 10:04:06 +0300

    Delete console.log from main.js

--- src/main.js
+++ src/main.js
@@ -12,11 +12,9 @@ const pageController = new PageController(document.body, moviesModel, api);
 api.getMovies()
   .then((movies) => {
     moviesModel.movies = movies;
-    movies.forEach((movie) => console.log(movie.comments));
     return Promise.all(movies.map((movie) => api.getComments(movie.id)));
   })
   .then((comments) => {
     moviesModel.comments = comments.flat();
-    console.log(comments.flat());
     pageController.render();
   });
