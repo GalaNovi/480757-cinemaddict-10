8852440cb024d8a91fec79b53f7cf41ad98f3314
Oleg Novitsky <darkmus@tut.by>
2020-02-03 15:58:55 +0300

    Cache statics

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 import MoviesModel from './models/movies';
-import API from './api.js';
+import API from './api/index';
 import {PageController} from './controllers/page';
 
 const END_POINT = `https://htmlacademy-es-10.appspot.com/cinemaddict/`;
 const randomString = Math.random().toString(36).substring(2, 10);
-const authorizationCode = `Basic ${randomString}`;
+// const authorizationCode = `Basic ${randomString}`;
+const authorizationCode = `Basic kjasrhgfkuGKUGiYUUfIYtfjytf`;
 
 const api = new API(END_POINT, authorizationCode);
 const moviesModel = new MoviesModel(api);
 const pageController = new PageController(document.body, moviesModel);
+
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`)
+    .then(() => {
+      console.log(`REGISTERED`);
+    })
+    .catch(() => {
+      console.log(`NOT REGISTERED`);
+    });
+});
 
 moviesModel.getMovies()
   .then((movies) => Promise.all(movies.map((movie) => moviesModel.getComments(movie.id))))
